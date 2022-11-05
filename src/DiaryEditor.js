9edcfd9792669b5f77fc35af8556803a2973fed9
das0166 <das0166@naver.com>
2022-11-06 00:00:35 +0900

    [2022-11-05]오늘의 일기 작성

---
+++ src/DiaryEditor.js
@@ -0,0 +1,75 @@
+import { useState, useRef } from "react";
+
+const DiaryEditor = () => {
+    const authorinput = useRef(); 
+    const contentinput = useRef(); 
+
+    const [state, setState] = useState({
+        author : "",
+        content : "",
+        emotion : 1,
+    });
+
+    const handleChangeState = (e) => {
+        setState({
+            ...state,
+            [e.target.name] : e.target.value,
+        });
+    };
+
+    const handleSubmit = () => {
+        if (state.author.length < 1){
+            //focus
+            authorinput.current.focus();
+            return;
+        }
+
+        if (state.content.length < 5){
+            //focus
+            contentinput.current.focus();
+            return;
+        }
+
+        console.log(state);
+        alert('저장 성공');
+    };
+
+    return (
+    <div className="DiaryEditor">
+        <h2>오늘의 일기</h2>
+        <div>
+            <input
+            ref={authorinput}
+            name="author"
+            value={state.author}
+            onChange={handleChangeState}
+            />
+        </div>
+        <div>
+            <textarea
+            ref={contentinput}
+            name="content"
+            value={state.content}
+            onChange={handleChangeState}
+            />
+        </div>
+        <div>
+            <span>오늘의 감정 점수 : </span>
+            <select
+            name="emotion"
+            value={state.emotion}
+            onChange={handleChangeState}>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+            </select>
+        </div>
+        <div>
+            <button onClick={handleSubmit}>일기 저장하기</button>
+        </div>
+    </div>
+    );
+};
+export default DiaryEditor;
